a1b08cbbf102729e04d9106d503fe7c52a3ead01
Wandelson Soares <wandelson.soares@gmail.com>
2024-01-26 10:34:01 -0300

    Backend - Etapa 6

--- Domain/Interfaces/IUsuarioSistemaFinanceiro/InterfaceUsuarioSistemaFinanceiro.cs
+++ Domain/Interfaces/IUsuarioSistemaFinanceiro/InterfaceUsuarioSistemaFinanceiro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Domain.Interfaces.IUsuarioSistemaFinanceiro
 {
     public interface InterfaceUsuarioSistemaFinanceiro
     {
+        Task<IList<UsuarioSistemaFinanceiro>> ListarUsuariosSistema(int IdSistema);
+
+        Task RemoveUsuarios(List<UsuarioSistemaFinanceiro> usuarios);
+
+        Task<UsuarioSistemaFinanceiro> ObterUsuarioPorEmail (string emailUsuario);
     }
 }
