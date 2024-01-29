031392ad0f03c1014ca4ce0828104877ab2eeac5
Wandelson Soares <wandelson.soares@gmail.com>
2024-01-29 17:18:09 -0300

    Backend - Etapa 10

--- Domain/Interfaces/InterfaceServicos/IDespesaServico.cs
+++ Domain/Interfaces/InterfaceServicos/IDespesaServico.cs
@@ -12,5 +12,8 @@ namespace Domain.Interfaces.InterfaceServicos
         Task AdicionarDespesa(Despesa despesa);
 
         Task AtualizarDespesa(Despesa despesa);
+
+        Task<object> CarregaGraficos(string emailUsuario);
+
     }
 }
--- Domain/Servicos/DespesaServico.cs
+++ Domain/Servicos/DespesaServico.cs
@@ -37,7 +37,7 @@ namespace Domain.Servicos
         {
             var data = DateTime.UtcNow;
             despesa.DataAlteracao = data;
-            
+
             if (despesa.Pago)
             {
                 despesa.DataPagamento = data;
@@ -48,6 +48,31 @@ namespace Domain.Servicos
             {
                 await _interfaceDespesa.Update(despesa);
             }
+        }
+
+        public async Task<object> CarregaGraficos(string emailUsuario)
+        {
+            var despesasUsuario = await _interfaceDespesa.ListarDespesasUsuario(emailUsuario);
+            var despesasAnterior = await _interfaceDespesa.ListarDespesasUsuarioNaoPagasMesesAnterior(emailUsuario);
+            var despesas_naoPagasMesesAnteriores = despesasAnterior.Any() ?
+                                                    despesasAnterior.ToList().Sum(x => x.Valor) :
+                                                    0;
+            var despesas_pagas = despesasUsuario.Where(d => d.Pago && d.TipoDespesa == Entities.Enums.EnumTipoDespesa.Contas)
+                                                .Sum(x => x.Valor);
+
+            var despesas_pendentes = despesasUsuario.Where(d => !d.Pago && d.TipoDespesa == Entities.Enums.EnumTipoDespesa.Contas)
+                                                .Sum(x => x.Valor);
+
+            var investimentos = despesasUsuario.Where(d => d.TipoDespesa == Entities.Enums.EnumTipoDespesa.Investimento)
+                                                .Sum(x => x.Valor);
+
+            return new
+            {
+                sucesso = "OK",
+                despesas_pagas = despesas_pagas,
+                despesas_pendentes = despesas_pendentes,
+                investimentos = investimentos
+            };
         }
     }
 }
